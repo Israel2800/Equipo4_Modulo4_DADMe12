975d4e59fd5d03306e10be925c11cd8d1883a2f2
Israel2800 <dady281100@gmail.com>
2024-08-07 01:16:20 -0500

    First activity added from second exercise

---
+++ app/src/main/java/com/israelaguilar/androidbasic12/ejerciciosclase/ejercicios2/actividad/UpdateInformationActivity.kt
@@ -0,0 +1,16 @@
+package com.israelaguilar.androidbasic12.ejerciciosclase.ejercicios2.actividad
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.israelaguilar.androidbasic12.R
+
+class UpdateInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_information)
+
+    }
+}
