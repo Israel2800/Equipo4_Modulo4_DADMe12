51f0489f38a45662241198c6cbce376c476a1351
Israel2800 <dady281100@gmail.com>
2024-08-07 01:32:15 -0500

    Menu activity has been added

--- app/src/main/java/com/israelaguilar/androidbasic12/ejerciciosclase/ejercicios2/actividad/HomeComponentsActivity.kt
+++ app/src/main/java/com/israelaguilar/androidbasic12/ejerciciosclase/ejercicios2/actividad/HomeComponentsActivity.kt
@@ -1,6 +1,9 @@
 package com.israelaguilar.androidbasic12.ejerciciosclase.ejercicios2.actividad
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +13,16 @@ import com.israelaguilar.androidbasic12.R
 class HomeComponentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home_components)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val imageBtnActivity1 = findViewById<ImageButton>(R.id.imageBtnActivity1)
+
+        imageBtnActivity1.setOnClickListener {
+            val intent = Intent(this, UpdateInformationActivity::class.java).apply {
+            }
+            startActivity(intent)
+
         }
+
     }
 }
