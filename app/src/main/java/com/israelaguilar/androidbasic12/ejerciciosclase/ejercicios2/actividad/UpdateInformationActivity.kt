1fecfb84e9a38c3a5771c62279a9996d33608a53
Israel2800 <dady281100@gmail.com>
2024-08-07 21:53:00 -0500

    Adding spinner to UpdateInformationActivity

--- app/src/main/java/com/israelaguilar/androidbasic12/ejerciciosclase/ejercicios2/actividad/UpdateInformationActivity.kt
+++ app/src/main/java/com/israelaguilar/androidbasic12/ejerciciosclase/ejercicios2/actividad/UpdateInformationActivity.kt
@@ -1,6 +1,8 @@
 package com.israelaguilar.androidbasic12.ejerciciosclase.ejercicios2.actividad
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,52 @@ class UpdateInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_information)
+
+        val spinnerMexicanStates = findViewById<Spinner>(R.id.spinnerMexicanStates)
+
+
+        //SPINNER
+        val data = arrayListOf(
+            "No selección",
+            "Aguascalientes",
+            "Baja California",
+            "Baja California Sur",
+            "Campeche",
+            "Chiapas",
+            "Chihuahua",
+            "Coahuila",
+            "Colima",
+            "Durango",
+            "Guanajuato",
+            "Guerrero",
+            "Hidalgo",
+            "Jalisco",
+            "México",
+            "Michoacán",
+            "Morelos",
+            "Nayarit",
+            "Nuevo León",
+            "Oaxaca",
+            "Puebla",
+            "Querétaro",
+            "Quintana Roo",
+            "San Luis Potosí",
+            "Sinaloa",
+            "Sonora",
+            "Tabasco",
+            "Tamaulipas",
+            "Tlaxcala",
+            "Veracruz",
+            "Yucatán",
+            "Zacatecas",
+            "Ciudad de México"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, data).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+
+        spinnerMexicanStates.adapter = adapter
 
     }
 }
