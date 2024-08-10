e9eaa5e3f0a8bb8b34523394fcc22ff253cbeab5
luzitzelvh <li.valdeolivar@gmail.com>
2024-08-09 23:00:27 -0600

    vista de registro

--- app/src/main/java/com/israelaguilar/androidbasic12/tarea/itzel/SingUpActivity.kt
+++ app/src/main/java/com/israelaguilar/androidbasic12/tarea/itzel/SingUpActivity.kt
@@ -12,10 +12,9 @@ class SingUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sing_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
