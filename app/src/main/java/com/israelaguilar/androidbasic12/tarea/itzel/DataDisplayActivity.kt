73d8143f334fd6edca3262b39ff80871ea1c7d3e
luzitzelvh <li.valdeolivar@gmail.com>
2024-08-10 00:48:11 -0600

    Mostrar info registrada

--- app/src/main/java/com/israelaguilar/androidbasic12/tarea/itzel/DataDisplayActivity.kt
+++ app/src/main/java/com/israelaguilar/androidbasic12/tarea/itzel/DataDisplayActivity.kt
@@ -1,6 +1,8 @@
 package com.israelaguilar.androidbasic12.tarea.itzel
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +12,33 @@ import com.israelaguilar.androidbasic12.R
 class DataDisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setContentView(R.layout.activity_data_display)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        val name = intent.getStringExtra("USER_NAME")
+        val lastname = intent.getStringExtra("USER_LASTNAME")
+        val email = intent.getStringExtra("USER_EMAIL")
+        val password = intent.getStringExtra("USER_PASSWORD")
+        val gender = intent.getStringExtra("USER_GENDER")
+        val month = intent.getStringExtra("USER_MONTH")
+
+        findViewById<TextView>(R.id.tvNameOut).text = "Nombre: $name"
+        findViewById<TextView>(R.id.tvLNameOut).text = "Apellido: $lastname"
+        findViewById<TextView>(R.id.tvEmailOut).text = "$email"
+        findViewById<TextView>(R.id.tvPasswordOut).text = "$password"
+        findViewById<TextView>(R.id.tvGenderOut).text = "Sexo: $gender"
+        findViewById<TextView>(R.id.tvMonthOut).text = "Mes de nacimiento: $month"
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/com/israelaguilar/androidbasic12/tarea/itzel/SingUpActivity.kt
+++ app/src/main/java/com/israelaguilar/androidbasic12/tarea/itzel/SingUpActivity.kt
@@ -1,6 +1,16 @@
 package com.israelaguilar.androidbasic12.tarea.itzel
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,13 +18,98 @@ import androidx.core.view.WindowInsetsCompat
 import com.israelaguilar.androidbasic12.R
 
 class SingUpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sing_up)
 
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val etName: EditText = findViewById(R.id.etName)
+        val etLName: EditText = findViewById(R.id.etLName)
+        val etEmail: EditText = findViewById(R.id.etEmail)
+        val etPassword: EditText = findViewById(R.id.etPassword)
+        val rgGender: RadioGroup = findViewById(R.id.rgGender)
+        val cbTermsAndConds : CheckBox = findViewById(R.id.cbTermsAnsConds)
+        val spinnerMonth : Spinner = findViewById(R.id.spinnerMonth)
+        val btnSingUp : Button = findViewById((R.id.btnSignUp))
+
+        val spinnerMonthData = findViewById<Spinner>(R.id.spinnerMonth)
+
+        val data = arrayListOf(
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, data).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        }
+        spinnerMonthData.adapter = adapter
+
+        btnSingUp.setOnClickListener {
+            if(validateInfo(etName,etLName,etEmail,etPassword,rgGender,cbTermsAndConds)){
+                val name = etName.text.toString()
+                val lastname = etLName.text.toString()
+                val email = etEmail.text.toString()
+                val password = etPassword.text.toString()
+                val month = spinnerMonth.selectedItem.toString()
+                val gender = findViewById<RadioButton>(rgGender.checkedRadioButtonId)?.text.toString()
+
+
+                val intent = Intent(this, DataDisplayActivity::class.java).apply {
+                    putExtra("USER_NAME", name)
+                    putExtra("USER_LASTNAME", lastname)
+                    putExtra("USER_EMAIL", email)
+                    putExtra("USER_PASSWORD", password)
+                    putExtra("USER_GENDER", gender)
+                    putExtra("USER_MONTH", month)
+
+                }
+                startActivity(intent)
+
+            }else{
+                Toast.makeText(this, "Debe llenar todos los campos antes de continuar", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
     }
+
+    private fun validateInfo(
+        etName:EditText,
+        etLName:EditText,
+        etEmail:EditText,
+        etPassword:EditText,
+        rgGender:RadioGroup,
+        cbTermsAndConds:CheckBox):
+        Boolean{
+            return etName.text.isNotEmpty() &&
+                    etLName.text.isNotEmpty() &&
+                    etEmail.text.isNotEmpty() &&
+                    etPassword.text.isNotEmpty() &&
+                    rgGender.checkedRadioButtonId != -1 &&
+                    cbTermsAndConds.isChecked
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
